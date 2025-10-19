c926ff0f48b70f341a7c395b309b776b01e365fa
fajoce <fajoce@gmail.com>
2025-10-19 18:58:08 -0500

    Agrgeue el cors de angular local

--- src/main/java/com/example/trafficapi/security/SecurityConfig.java
+++ src/main/java/com/example/trafficapi/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://kanbas.netlify.app")); // tu frontend
+        configuration.setAllowedOrigins(List.of("https://kanbas.netlify.app,http://localhost:4200")); // tu frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
